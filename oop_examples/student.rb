372098d2c733dd86936b14b5b3130df6bcdb732f
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-03-27 08:20:50 +0900

    class example

---
+++ oop_examples/student.rb
@@ -0,0 +1,43 @@
+class Student
+  attr_accessor :first_name, :last_name, :email, :password
+  attr_reader :username
+
+  @first_name
+  @last_name
+  @email
+  @username
+  @password
+
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+
+  # redundant code
+  # def first_name=(name)
+  #   @first_name = name
+  # end
+  #
+  # def first_name
+  #   @first_name
+  # end
+
+  def to_s
+    "first name: #{@first_name}, last name: #{@last_name},
+      username: #{@username}, email: #{@email}, password: #{@password}"
+  end
+
+end
+
+mike = Student.new("mike", "popcorn", "mikepopcorn", "mike@example.com", "passw0rd")
+mike.password = "updated_password"
+
+puts mike
+puts mike.first_name
+puts mike.last_name
+puts mike.email
+puts mike.username
+puts mike.password
