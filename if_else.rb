874ac038fdc5e14974c4279e99b0af5eaa85ab8a
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-03-26 15:48:00 +0900

    add method, if else

---
+++ if_else.rb
@@ -0,0 +1,19 @@
+condition = true
+another_condition = false
+
+# if condition && another_condition
+if condition && !another_condition
+  puts "hello"
+else
+  puts "good bye"
+end
+
+name = "popcorn"
+
+if name == "mike"
+  puts "hello mike"
+elsif name == "popcorn"
+  puts "hello popcorn"
+else
+  puts "hello unknown"
+end
---
+++ methods.rb
@@ -0,0 +1,8 @@
+def multiply(first_num, second_num)
+  # return is automatically implied
+  first_num.to_f * second_num.to_f
+end
+
+first_number = 3
+second_number = 5
+puts "The first number multiplied by the second number is: #{multiply(first_number, second_number)}"
