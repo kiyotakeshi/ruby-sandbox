f0fa3e10b4701edd3f716dd393b3d3cdaebb795f
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-03-26 19:24:28 +0900

    polish authentication

--- authenticator.rb
+++ authenticator.rb
@@ -6,23 +6,26 @@ users = [
   { username: "west", password: "password4" },
 ]
 
+def auth_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record
+    end
+  end
+  "credentials were not correct"
+end
+
 puts "let's enter your username and password"
 
-attempts = 0
-while attempts < 3
+attempts = 1
+while attempts < 4
   print "username: "
   username = gets.chomp
   print "password: "
   password = gets.chomp
 
-  users.each { |user|
-    if user[:username] == username && user[:password] == password
-      puts user
-      break
-    else
-      puts "credentials were not correct"
-    end
-  }
+  authentication = auth_user(username, password, users)
+  puts authentication
 
   puts "press n to quit or any other key to continue: "
   input = gets.chomp.downcase
@@ -33,3 +36,4 @@ while attempts < 3
 
   attempts += 1
 end
+puts "you have exceeded the number of attempts" if attempts == 4
